e040890840ef21d8bc80f18dbc4d9af5278f876e
takabu-kouki <146901437+takabu-kouki@users.noreply.github.com>
2024-06-14 23:43:40 +0900

    プロジェクト ファイルを追加します。

---
+++ Enemy.cpp
@@ -0,0 +1,22 @@
+//#include "Enemy.h"
+//
+//void Enemy::Update() {
+//    (this->*currentState)();
+//
+//}
+//void Enemy::approach()//Ú‹ß
+//{
+//     std::cout << "“G‚ÌÚ‹ßI" << std::endl;
+//}
+//
+//void Enemy::shoot()
+//{
+//    std::cout << "“G‚ÌUŒ‚I" << std::endl;
+//
+//}
+//
+//void Enemy::retreat()
+//{
+//    std::cout << "“G‚ÌŒã‘ŞI" << std::endl;
+//
+//}
---
+++ Enemy.h
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//class Enemy {
+//public:
+//    // 状態遷移関数ポインタの定義
+//    typedef void (Enemy::* StateFunction)();
+//
+//    Enemy();
+//
+//    // 状態遷移を実行するメンバ関数
+//    void Update();
+//     void approach();
+//    void shoot();
+//    void retreat();
+//private:
+//    // 状態遷移のためのメンバ関数
+//   
+//    StateFunction currentState;
+//};
